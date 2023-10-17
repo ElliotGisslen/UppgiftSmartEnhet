d4bf1a529d055efda8c818bc06c23c992cbab688
ElliotGisslen <113430957+ElliotGisslen@users.noreply.github.com>
2023-10-17 05:26:58 +0200

    Add files via upload

---
+++ Actions/SaveToCosmosDb.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Text;
+using Azure.Messaging.EventHubs;
+using AzureFunctions.Contexts;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace AzureFunctions.Actions
+{
+    public class SaveToCosmosDb
+    {
+        private readonly CosmosContext _context;
+
+        public SaveToCosmosDb(CosmosContext context)
+        {
+            _context = context;
+        }
+
+        [Function(nameof(SaveToCosmosDb))]
+        public async Task Run([EventHubTrigger("iothub-ehub-elliot-iot-25234255-8916e93e4b", Connection = "IotHubEndpoint")] EventData[] events)
+        {
+            foreach (EventData @event in events)
+            {
+                var deviceMessage = new Models.DeviceItemMessage
+                {
+                    DeviceId = @event.SystemProperties["iothub-connection-device-id"].ToString(),
+                    Payload = Encoding.UTF8.GetString(@event.EventBody.ToArray())
+                };
+                _context.Messages.Add(deviceMessage);
+                await _context.SaveChangesAsync();
+
+            }
+
+        }
+    }
+}
